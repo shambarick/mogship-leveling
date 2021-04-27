b12e24cc45c30011f977b6840d7abdf3fa454b55
shambarick <shambarick@users.noreply.github.com>
2021-04-26 18:17:17 -1000

    sort server side and fix a few things

--- app/api/api_v1/endpoints/routes.py
+++ app/api/api_v1/endpoints/routes.py
@@ -8,7 +8,7 @@ from app.api.deps import (
     parse_list_exclude_sectors,
     parse_list_include_sectors,
     parse_list_maps,
-    parse_list_include_maps
+    parse_list_include_maps,
 )
 from app.models.route import SubmarineRoute
 from app.core.elasticsearch import get_client
@@ -42,8 +42,8 @@ def get_range_query(value, field_name):
 
 @router.get("", response_model=List[SubmarineRoute])
 async def get_routes(
-    required_rank: int = Query(1, alias="rank", ge=1, le=90),
-    required_range: int = Query(70, alias="range"),
+    required_rank: Optional[int] = Query(None, alias="rank", ge=1, le=90),
+    required_range: Optional[int] = Query(None, alias="range"),
     size: int = Query(100, ge=1),
     exp: Optional[int] = Query(None, alias="exp"),
     max_distance: Optional[int] = Query(None, alias="maxDistance"),
@@ -52,6 +52,7 @@ async def get_routes(
     maps: Optional[List[str]] = Depends(parse_list_maps),
     include_maps: Optional[List[str]] = Depends(parse_list_include_maps),
     exclude_maps: Optional[List[str]] = Depends(parse_list_exclude_maps),
+    sort: str = Query('Exp,desc,Distance,asc'),
 ):
     query_max_distance = [get_range_query(max_distance, 'Distance')] if max_distance and max_distance > 0 else []
     query_include_sectors = get_term_query_from_list(include_sectors, 'Route')
@@ -66,6 +67,41 @@ async def get_routes(
             },
         },
     }] if exp is not None else []
+    query_rank = [{
+        'range': {
+            'Rank': {
+                'lte': required_rank,
+            },
+        },
+    }] if required_rank is not None else []
+    query_range = [{
+        'range': {
+            'Range': {
+                'lte': required_range,
+            },
+        },
+    }] if required_range is not None else []
+
+    es_sort = []
+    sort_list = sort.split(",")
+    if len(sort_list)%2 == 0:
+        for i in range(0, len(sort_list), 2):
+            es_sort.append({
+                sort_list[i]: {
+                    "order": sort_list[i + 1]
+                }
+            })
+    else:
+        es_sort.append({
+            "Exp": {
+                "order": "desc",
+            }
+        })
+        es_sort.append({
+            "Distance": {
+                "order": "asc",
+            }
+        })
 
     query = {
             'size': size,
@@ -75,20 +111,8 @@ async def get_routes(
                         *query_maps
                     ],
                     'filter': [
-                        {
-                            'range': {
-                                'Rank': {
-                                    'lte': required_rank
-                                }
-                            }
-                        },
-                        {
-                            'range': {
-                                'Range': {
-                                    'lte': required_range
-                                }
-                            }
-                        },
+                        *query_rank,
+                        *query_range,
                         *query_exp,
                         *query_max_distance,
                         *query_include_sectors,
@@ -100,19 +124,9 @@ async def get_routes(
                     ]
                 }
             },
-            'sort': [
-                {
-                    'Exp': {
-                        'order': 'DESC'
-                    }
-                },
-                {
-                    'Distance': {
-                        'order': 'ASC'
-                    }
-                },
-            ]
+            'sort': es_sort,
     }
+    logger.info(query)
 
     results = await get_client().search(
         index="submarineleveling",
--- app/models/route.py
+++ app/models/route.py
@@ -1,21 +1,21 @@
-from typing import List
+from typing import List, Optional
 from pydantic import BaseModel, Field
 
 
 class SubmarineSector(BaseModel):
     name: str = Field(alias="Name")
-    map_id: int = Field(alias="MapID")
-    map_name: dict = Field(alias="Map")
-    required_rank: int = Field(alias="Rank")
-    sector_id: int = Field(alias="SectorId")
-    alias_name: str = Field(alias="AliasName")
-    exp: int = Field(alias="Exp")
+    map_id: Optional[int] = Field(alias="MapID")
+    map_name: Optional[dict] = Field(alias="Map")
+    required_rank: Optional[int] = Field(alias="Rank")
+    sector_id: Optional[int] = Field(alias="SectorId")
+    alias_name: Optional[str] = Field(alias="AliasName")
+    exp: Optional[int] = Field(alias="Exp")
 
 
 class SubmarineRoute(BaseModel):
     required_rank: int = Field(alias="Rank")
     map_id: int = Field(alias="MapID")
-    map_name: dict = Field(alias="Map")
+    map_name: Optional[dict] = Field(alias="Map")
     distance: int = Field(alias="Distance")
     sectors_total: int = Field(alias="SectorsTotal")
     exp: int = Field(alias="Exp")
